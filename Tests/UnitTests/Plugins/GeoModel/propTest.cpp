05e9ef96d631a73d86adad93a4f36eec04218b6b
Carl Jonas Berggren <cberggre@atlmuonsw1.dyndns.cern.ch>
2024-09-12 11:14:32 +0200

    working skeleton

--- Tests/UnitTests/Plugins/GeoModel/propTest.cpp
+++ Tests/UnitTests/Plugins/GeoModel/propTest.cpp
@@ -26,68 +26,52 @@
 #include "Acts/Propagator/EigenStepper.hpp"
 #include "Acts/Propagator/Propagator.hpp"
 #include "Acts/Propagator/SurfaceCollector.hpp"
+#include "ActsExamples/Generators/ParametricParticleGenerator.hpp"
+//#include "ActsExamples/MuonSpectrometerMockupDetector/MockupSectorBuilder.hpp"
 
 BOOST_AUTO_TEST_SUITE(GeoModelPlugin)
 
+Acts::GeometryContext gContext;
 
 
 
 BOOST_AUTO_TEST_CASE(proTest) {
-  // create pars for conversion
-  Acts::GeoModelDetectorObjectFactory::Config gmConfig;
-  Acts::GeoModelDetectorObjectFactory::Options gmOptions;
-  gmConfig.convertBox = {"Box"};
-  Acts::GeometryContext gContext;
-  Acts::MagneticFieldContext mfContext;
-  Acts::GeoModelDetectorObjectFactory::Cache gmCache;
-
-  // create factory instance
-  Acts::GeoModelDetectorObjectFactory factory(gmConfig);
-  auto gmTree = Acts::GeoModelReader::readFromDb("~/ATLAS-R3-MUONTEST_v3.db");
-  factory.construct(gmCache, gContext, gmTree, gmOptions);
-  const auto gmBoxes = gmCache.boundingBoxes;
-  const auto gmSensSurfaces = gmCache.sensitiveSurfaces;
-
-  std::vector<std::shared_ptr<Acts::Surface>> gmSurfaces(gmSensSurfaces.size());
-  std::transform(gmSensSurfaces.begin(), gmSensSurfaces.end(), gmSurfaces.begin(),
-                 [](const std::tuple<std::shared_ptr<Acts::GeoModelDetectorElement>,
-                                     std::shared_ptr<Acts::Surface>>& t) {
-                   return std::get<1>(t);
-                 });
-
-  auto bounds = std::make_shared<Acts::CylinderVolumeBounds>(0, 15000, 25000);
+  auto bounds = std::make_unique<Acts::CylinderVolumeBounds>(
+      0, 15000, 25000);
   auto worldVolume = Acts::Experimental::DetectorVolumeFactory::construct(
       Acts::Experimental::defaultPortalAndSubPortalGenerator(), gContext,
       "World_Detector_Volume",
       Acts::Transform3(Acts::Transform3::Identity() *
                  Acts::AngleAxis3(M_PI / 2, Acts::Vector3(0., 0., 1))),
-      bounds, gmSurfaces, gmBoxes,
+      std::move(bounds), std::vector<std::shared_ptr<Acts::Surface>>{},
+      std::vector<std::shared_ptr<Acts::Experimental::DetectorVolume>>{},
       Acts::Experimental::tryAllSubVolumes(),
       Acts::Experimental::tryAllPortalsAndSurfaces());
-
-  worldVolume->assignGeometryId(Acts::GeometryIdentifier{}.setVolume(250));//TODO what is that?
-
-  auto detector_sector = Acts::Experimental::Detector::makeShared(
-      "Detector", {worldVolume}, Acts::Experimental::tryRootVolumes());
-
-  // Set the navigator for the propagator
-  Acts::Experimental::DetectorNavigator::Config navCfg;
-  navCfg.detector = detector_sector.get();
-  auto navigator = Acts::Experimental::DetectorNavigator(
-      navCfg, Acts::getDefaultLogger("DetectorNavigator",
-                                     Acts::Logging::Level::WARNING));
-  // Set the stepper for the propagator with a magnetic field
-  auto bField = std::make_shared<Acts::ConstantBField>(
-      Acts::Vector3{0 * Acts::UnitConstants::T, 0, 0});
-  auto stepper = Acts::EigenStepper<>(bField);
-
-  // Set the propagator options and the propagator
-  auto options =
-      Acts::PropagatorOptions<Acts::ActionList, Acts::AbortList>(gContext, mfContext);
-  auto propagator =
-      Acts::Propagator<Acts::EigenStepper<>, Acts::Experimental::DetectorNavigator>(
-          stepper, navigator,
-          Acts::getDefaultLogger("Propagator", Acts::Logging::Level::WARNING));
+  //worldVolume->assignGeometryId(Acts::GeometryIdentifier{}.setVolume(250));
+  auto rMax = worldVolume->volumeBounds().values()[1];
+  auto hlengthZ = worldVolume->volumeBounds().values()[2];
+  float theta = std::acos(hlengthZ / rMax);
+  std::vector<float> pTValue = {1.};
+  for (std::size_t i = 0; i < pTValue.size(); i++) {
+    ActsExamples::ParametricParticleGenerator::Config pCfg;
+    pCfg.thetaMin = theta;
+    pCfg.thetaMax = M_PI - theta;
+    ActsExamples::WhiteBoard eventStore(
+        Acts::getDefaultLogger("Event_Store", Acts::Logging::Level::WARNING),
+        {{"Particles_pT", std::to_string(pTValue[i])}});
+    int njobs = 100;
+    for (int nj = 0; nj < njobs; nj++) {
+      ActsExamples::ParametricParticleGenerator pgenerator{pCfg};
+      auto rnd = std::make_shared<ActsExamples::RandomNumbers>(
+          ActsExamples::RandomNumbers::Config{nj});
+      ActsExamples::AlgorithmContext alContext(0, i, eventStore);
+      ActsExamples::RandomEngine randomEng = rnd->spawnGenerator(alContext);
+      auto particles = pgenerator(randomEng);
+      /*
+      for (auto ip : particles) {
+      }
+      */
+    }
+  }
 }
-
 BOOST_AUTO_TEST_SUITE_END()
